6f83c52e65940558a90376c6ba4b3f919086726f
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-15 18:22:40 +0200

    JSX and React.createElement

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>Hej React</h1>
+        <h1>Hej React</h1>
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hej React'));
   }
 }
 
