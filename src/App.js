5ba0aededc9fd6944a7b78900da75a56ec3ab735
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-16 12:36:29 +0200

    Click Events and functions

--- src/App.js
+++ src/App.js
@@ -3,14 +3,46 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: 'Hannes', age: 31 },
+      { name: 'Emma', age: 27 },
+      { name: 'Marit', age: 21 }
+    ],
+    otherState: 'Some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Clicki click!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Hones';
+    this.setState({
+      persons: [
+        { name: newName, age: 31 },
+        { name: 'Emma', age: 27 },
+        { name: 'Marit', age: 81 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hej React</h1>
         <p>Paragraf</p>
-        <Person name="Hannes" age="23" />
-        <Person name="Emma" age="25">My Hobbies: Racing</Person>
-        <Person name="Marit" age="27" />
+        <button onClick={ () => this.switchNameHandler('SwitchHannes') }>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Hunnes')}>
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hej React'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,10 @@ const person = (props) => {
   const randomAge = Math.floor(Math.random() * 30);
 
   return (
-    <p>My name is {props.name}, and I am {props.age} years old (previously {randomAge})</p>
+    <div>
+      <p onClick={props.click}>My name is {props.name}, and I am {props.age} years old (previously {randomAge})</p>
+      <p>{props.children}</p>
+    </div>
   );
 }
 
