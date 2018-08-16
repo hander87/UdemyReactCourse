97716397d6e77ab2c6ec85bcff36d7acd9940d60
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-16 16:52:15 +0200

    Outsourcing IF Statements Outside Render

--- src/App.js
+++ src/App.js
@@ -53,35 +53,38 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Hunnes')}
+            changed={this.nameChangeHandler}>
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hej React</h1>
         <p>Letsa go!</p>
         <button
           style={ourStyle}
-          onClick={this.togglePersonsHandler}
-          >
-          Show Persons
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons
         </button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age} />
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Hunnes')}
-                changed={this.nameChangeHandler}>
-                My Hobbies: Racing
-              </Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age} />
-            </div>
-          : null
-        }
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hej React'));
