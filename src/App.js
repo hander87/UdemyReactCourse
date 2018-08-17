21da9cd1f2b6ecfd2d3a1ba032663e6759a5c43a
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-17 09:07:09 +0200

    Assignment Init

--- src/App.js
+++ src/App.js
@@ -1,91 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
 
 class App extends Component {
-
-  state = {
-    persons: [
-      { id: '01kjdhssdfa',name: 'Hannes', age: 31 },
-      { id: '02slkjdfhic',name: 'Emma', age: 27 },
-      { id: '03jksdaksdd',name: 'Marit', age: 21 }
-    ],
-    otherState: 'Some other value',
-    showPersons: false
-  }
-
-  nameChangeHandler = (event, id) => {
-    const personInd = this.state.persons.findIndex( (p) => {
-      p.id === id;
-    });
-
-    const person = {
-      ...this.state.persons[personInd]
-    };
-    // const person = Object.assign({}, this.state.persons[personInd]);
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personInd] = person;
-
-    this.setState( {perons: persons} )
-  }
-
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
-    const persons = [... this.state.persons]
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  }
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({
-      showPersons: !doesShow
-    });
-  }
-
   render() {
-
-    const ourStyle = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
-    let persons = null;
-
-    if(this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangeHandler(event, person.id)}/>
-          })}
-        </div>
-      );
-    }
-
     return (
       <div className="App">
-        <h1>Hej React</h1>
-        <p>Letsa go!</p>
-        <button
-          style={ourStyle}
-          onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-        {persons}
+        <ol>
+          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+          <li>When you click a CharComponent, it should be removed from the entered text.</li>
+        </ol>
+        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hej React'));
   }
 }
 
