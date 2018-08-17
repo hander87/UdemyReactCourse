cacb57833bcb7e47fc770f7c152cd825ce9d02d3
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-17 18:05:42 +0200

    Arranged Code into Components and Containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import React, { Component } from "react";
+
+import classes from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
-
   state = {
     persons: [
-      { id: '01kjdhssdfa',name: 'Hannes', age: 31 },
-      { id: '02slkjdfhic',name: 'Emma', age: 27 },
-      { id: '03jksdaksdd',name: 'Marit', age: 21 }
+      { id: "01kjdhssdfa", name: "Hannes", age: 31 },
+      { id: "02slkjdfhic", name: "Emma", age: 27 },
+      { id: "03jksdaksdd", name: "Marit", age: 21 }
     ],
-    otherState: 'Some other value',
+    otherState: "Some other value",
     showPersons: false
-  }
+  };
 
   nameChangeHandler = (event, id) => {
-    const personInd = this.state.persons.findIndex( (p) => {
+    const personInd = this.state.persons.findIndex(p => {
       return p.id === id;
     });
 
@@ -29,62 +30,43 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personInd] = person;
 
-    this.setState( {perons: persons} )
-  }
+    this.setState({ perons: persons });
+  };
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({
       showPersons: !doesShow
     });
-  }
+  };
 
   render() {
     let persons = null;
-    let btnClass = '';
 
-    if(this.state.showPersons) {
+    if (this.state.showPersons) {
       persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              changed={(event) => this.nameChangeHandler(event, person.id)}/>
-          );
-          })}
-        </div>
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangeHandler}
+        />
       );
-      btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.redtext);
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
     }
 
     return (
       <div className={classes.App}>
-        <h1>Hej React</h1>
-        <p className={assignedClasses.join(' ')}>Letsa go!</p>
-        <button
-          className={btnClass}
-          onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
